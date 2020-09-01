5ea84465eb1bd5b7cc538ae591e7f57f17c6f208
Sergei <black_white2018@rambler.ru>
2020-09-01 12:43:00 +0300

    Updates

---
+++ precache-manifest.c8b6a699fe4d9973c6c846de90105ab4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e8410b1b5367431c453b7a1068071be",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "4aad5b9af8c8861e5036",
+    "url": "/movies-app/static/css/1.c6cfea1d.chunk.css"
+  },
+  {
+    "revision": "5a94abed669ff33fa58d",
+    "url": "/movies-app/static/css/4.847e01e0.chunk.css"
+  },
+  {
+    "revision": "e0a0ef98475aaa14c75e",
+    "url": "/movies-app/static/css/main.10f6f0bb.chunk.css"
+  },
+  {
+    "revision": "c0d00561c778ba593a26",
+    "url": "/movies-app/static/js/0.8a0ec1bb.chunk.js"
+  },
+  {
+    "revision": "4aad5b9af8c8861e5036",
+    "url": "/movies-app/static/js/1.20885775.chunk.js"
+  },
+  {
+    "revision": "5a94abed669ff33fa58d",
+    "url": "/movies-app/static/js/4.6771bdc9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/movies-app/static/js/4.6771bdc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0a0ef98475aaa14c75e",
+    "url": "/movies-app/static/js/main.65acd857.chunk.js"
+  },
+  {
+    "revision": "d6e978a6b3843069162e",
+    "url": "/movies-app/static/js/runtime-main.0ae1c201.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movies-app/precache-manifest.6d2a65fcc53654537ca40069a5a15e80.js"
+  "/movies-app/precache-manifest.c8b6a699fe4d9973c6c846de90105ab4.js"
 );
 
 self.addEventListener('message', (event) => {
